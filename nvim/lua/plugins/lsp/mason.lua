d67d49314e4b5884ec8432a7f028b157b27d0dfd
hidetsumi <hidetsu99@gmail.com>
2024-11-22 23:41:45 -0300

    Basic dotfiles config
    ToDo:
    
    [] Improve LSP for ts/js/tsx/jsx
    [] Make Mason autodownload all the formatters and LSP

---
+++ nvim/lua/plugins/lsp/mason.lua
@@ -0,0 +1,37 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    require("mason").setup()
+
+    require("mason-lspconfig").setup({
+      automatic_installation = true,
+      ensure_installed = {
+        "cssls",
+        "eslint",
+        "html",
+        "jsonls",
+        "tsserver",
+        "pyright",
+        "tailwindcss",
+        automatic_installation = true,
+      },
+    })
+
+    require("mason-tool-installer").setup({
+      ensure_installed = {
+        "prettierd",
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "pylint",
+        "eslint_d",
+      },
+      auto_update = true,
+      run_on_start = true,
+    })
+  end,
+}
---
+++ nvim/lua/plugins/ui/lualine.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-lualine/lualine.nvim", -- Plugin de barra de estado
+  opts = {
+    options = {
+      section_separators = { left = "", right = "" }, -- Separadores visibles
+      theme = "gruvbox_dark", -- Cambia el tema de la barra de estado
+    },
+  },
+}
