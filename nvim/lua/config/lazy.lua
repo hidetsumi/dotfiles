8132759ba16a05d4dc92ae8c8e4ae5d28edf2dcf
hidetsumi <hidetsu99@gmail.com>
2025-02-09 14:17:33 -0300

    feat: Change to kanagawa theme in tmux

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -17,23 +17,27 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = {
         defaults = {
           keymaps = {
             format = false,
-          }
-        }
+          },
+        },
       },
       keys = {
         { "<leader>cf", false, mode = { "n", "v" } },
-      }, },
+      },
+    },
     -- import/override with your plugins
-    { import = "plugins.ui" },
-    { import = "plugins.colorscheme" },
-    { import = "plugins.lsp" },
-    { import = "plugins.navegation" },
-    { import = "plugins.ui" },
-    { import = "plugins.formatting" },
+    -- { import = "plugins.ui" },
+    -- { import = "plugins.colorscheme" },
+    -- { import = "plugins.lsp" },
+    -- { import = "plugins.navegation" },
+    -- { import = "plugins.ui" },
+    -- { import = "plugins.formatting" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
@@ -44,7 +48,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "rose-pine", "habamax" } },
+  install = { colorscheme = { "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
@@ -65,80 +69,3 @@ require("lazy").setup({
     },
   },
 })
-
-require("rose-pine").setup({
-  variant = "auto", -- auto, main, moon, or dawn
-  dark_variant = "main", -- main, moon, or dawn
-  dim_inactive_windows = false,
-  extend_background_behind_borders = true,
-
-  enable = {
-    terminal = true,
-    legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-    migrations = true, -- Handle deprecated options automatically
-  },
-
-  styles = {
-    bold = true,
-    italic = true,
-    transparency = false,
-  },
-
-  groups = {
-    border = "muted",
-    link = "iris",
-    panel = "surface",
-
-    error = "love",
-    hint = "iris",
-    info = "foam",
-    note = "pine",
-    todo = "rose",
-    warn = "gold",
-
-    git_add = "foam",
-    git_change = "rose",
-    git_delete = "love",
-    git_dirty = "rose",
-    git_ignore = "muted",
-    git_merge = "iris",
-    git_rename = "pine",
-    git_stage = "iris",
-    git_text = "rose",
-    git_untracked = "subtle",
-
-    h1 = "iris",
-    h2 = "foam",
-    h3 = "rose",
-    h4 = "gold",
-    h5 = "pine",
-    h6 = "foam",
-  },
-
-  palette = {
-    -- Override the builtin palette per variant
-    -- moon = {
-    --     base = '#18191a',
-    --     overlay = '#363738',
-    -- },
-  },
-
-  highlight_groups = {
-    -- Comment = { fg = "foam" },
-    -- VertSplit = { fg = "muted", bg = "muted" },
-  },
-
-  before_highlight = function(group, highlight, palette)
-    -- Disable all undercurls
-    -- if highlight.undercurl then
-    --     highlight.undercurl = false
-    -- end
-    --
-    -- Change palette colour
-    -- if highlight.fg == palette.pine then
-    --     highlight.fg = palette.foam
-    -- end
-  end,
-})
-
-vim.cmd("colorscheme rose-pine")
